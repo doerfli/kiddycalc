d2c8ba82363aafd3968a98a8ae2e8647cb1e2ba3
Marc Doerflinger <marc@doerf.li>
2022-09-24 22:03:46 +0000

    Add numbers

--- components/challenge.tsx
+++ components/challenge.tsx
@@ -1,4 +1,12 @@
 import React, { useState }  from "react";
+// import Number from "./number";
+import dynamic from 'next/dynamic'
+
+// TODO make ssr work again
+const Number = dynamic(
+    () => import('./number'),
+    { ssr: false }
+)
 
 interface ChallengeProps {
     number1: number;
@@ -10,8 +18,12 @@ export default function Challenge(props: ChallengeProps) {
     const [ number2, setNumber2 ] = useState(props.number2);
 
     return (
-        <div className="challenge">
-            
+        <div>
+            <Number number={number1} />
+            +
+            <Number number={number2} />
+            =
+            ?
         </div>
     );
 
---
+++ components/number.tsx
@@ -0,0 +1,28 @@
+import React, { useState }  from "react";
+
+import {
+    faCar,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+interface NumberProps {
+    number: number;
+}; 
+
+export default function Number(props: NumberProps) {
+    const [ number, setNumber ] = useState(props.number);
+
+    let icons = [];
+    for (let i = 0; i < number; i++) {
+        icons.push(<FontAwesomeIcon icon={faCar} />);
+    }
+
+    return (
+        <div>
+            {[...Array(number)].map((x, i) =>
+                <FontAwesomeIcon key={i} icon={faCar} />
+            )}
+        </div>
+    );
+
+}
