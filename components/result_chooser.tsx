869e8294c62ee03366e7b47610609335d9fcb6d6
Marc Doerflinger <marc@doerf.li>
2022-09-25 16:56:40 +0000

    shuffle choices

--- components/result_chooser.tsx
+++ components/result_chooser.tsx
@@ -1,5 +1,5 @@
 import React, { useState }  from "react";
-
+import arrayShuffle from 'array-shuffle';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface ResultChooserProps {
@@ -8,6 +8,7 @@ interface ResultChooserProps {
 
 export default function ResultChooser(props: ResultChooserProps) {
     const [ result, setResult ] = useState(props.result);
+    const [ choices, setChoices ] = useState(arrayShuffle([props.result, props.result + 1, props.result - 1]));
     const [ choice1, setChoice1 ] = useState(props.result);
     const [ choice2, setChoice2 ] = useState(props.result + 1);
     const [ choice3, setChoice3 ] = useState(props.result - 1);
@@ -16,21 +17,21 @@ export default function ResultChooser(props: ResultChooserProps) {
         <div className="flex flex-cols-3 gap-4 items-center">
             <div>
                 <div className="grid grid-cols-2 gap-4 p-4 mr-10 bg-red-300 rounded-lg">
-                    {[...Array(choice1)].map((x, i) =>
+                    {[...Array(choices[0])].map((x, i) =>
                         <FontAwesomeIcon key={i} icon='car' className="text-6xl" />
                     )}
                 </div>
             </div>
             <div>
                 <div className="grid grid-cols-2 gap-4 p-4 mr-10 bg-yellow-300 rounded-lg">
-                    {[...Array(choice2)].map((x, i) =>
+                    {[...Array(choices[1])].map((x, i) =>
                         <FontAwesomeIcon key={i} icon='car' className="text-6xl" />
                     )}
                 </div>
             </div>
             <div>
                 <div className="grid grid-cols-2 gap-4 p-4 mr-10 bg-blue-300 rounded-lg">
-                    {[...Array(choice3)].map((x, i) =>
+                    {[...Array(choices[2])].map((x, i) =>
                         <FontAwesomeIcon key={i} icon='car' className="text-6xl" />
                     )}
                 </div>
