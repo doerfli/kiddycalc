6327e0f2b481a8e3a58985224bbd21266f5b4a04
Marc Doerflinger <marc@doerf.li>
2022-09-25 16:31:13 +0000

    background color

--- components/number.tsx
+++ components/number.tsx
@@ -10,7 +10,7 @@ export default function Number(props: NumberProps) {
     const [ number, setNumber ] = useState(props.number);
 
     return (
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4 p-4 bg-green-300 rounded-lg">
             {[...Array(number)].map((x, i) =>
                 <FontAwesomeIcon key={i} icon='car' className="text-6xl" />
             )}
