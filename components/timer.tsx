d2de45c59355e956f8939b9dcb3250ff61c24c89
Marc Doerflinger <marc@doerf.li>
2022-09-30 21:17:33 +0000

    show timeout overlay only after selecting result (closes #16)

--- components/timer.tsx
+++ components/timer.tsx
@@ -1,15 +1,23 @@
-import React  from "react";
+import React, { useEffect }  from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import TimeoutOverlay from "./timeout_overlay";
 import TimerConfig from "./timer_config";
+import ChallengeSpecification from "../models/challenge_specification";
 
-export default function Timer() {
+interface TimerProps {
+    definition: ChallengeSpecification;
+}
 
+export default function Timer(props: TimerProps) {
+
+    const [ timerIconColor, setTimerIconColor ] = React.useState("text-neutral-800");
     const [ timerExpiration, setTimerExpiration ] = React.useState(0);
     const [ timerExpired, setTimerExpired ] = React.useState(false);
-    const [ showTimerConfig, setShowTimerConfig ] = React.useState(false);
-    const [ timerColor, setTimerColor ] = React.useState("text-neutral-800");
+    const [ showTimerExpiredOverlay, setShowTimerExpiredOverlay ] = React.useState(false);
+    const [ showTimerConfigOverlay, setShowTimerConfigOverlay ] = React.useState(false);
+    // keep track of the round to identify when it changed. Timer expired overlay should only be shown when round changes. 
+    const [ round, setRound ] = React.useState(props.definition.round);
     let intervalTimer: NodeJS.Timer;
 
     const configureTimer = () => {
@@ -17,21 +25,21 @@ export default function Timer() {
             return; // timer is already active
         }
 
-        setShowTimerConfig(true);
+        setShowTimerConfigOverlay(true);
     }
 
     const engageTimer = (minutes: number) => {
-        setShowTimerConfig(false);
+        setShowTimerConfigOverlay(false);
         console.log(minutes);
 
         if (minutes === 0) {
             return;
         }
 
-        const durationSeconds = minutes * 60;
+        const durationSeconds = minutes * 20;
         const exp = Date.now() + durationSeconds * 1000;
         setTimerExpiration(exp);
-        setTimerColor("text-emerald-500");
+        setTimerIconColor("text-emerald-500");
         console.log(`timer set to expire at ${exp}s`);
         
         intervalTimer = setInterval(() => {
@@ -39,24 +47,38 @@ export default function Timer() {
             console.log(`timeRemaining: ${timeRemaining}`);
             if (timeRemaining <= 0) {
                 setTimerExpired(true);
+                setTimerIconColor("text-red-500 animate-ping");
                 clearInterval(intervalTimer);
+            } else if (timeRemaining < 15) {
+                setTimerIconColor("text-red-500 animate-pulse");
             } else if (timeRemaining < 30) {
-                setTimerColor("text-red-500");
-            } else if (timeRemaining < 55) {
-                setTimerColor("text-yellow-500");
+                setTimerIconColor("text-red-500");
+            } else if (timeRemaining < 45) {
+                setTimerIconColor("text-yellow-500");
             }
         }, 1000);
     }
 
-    const timerClass = `fa-fw p-4 text-3xl ${timerColor}`;
+    useEffect(() => {
+        if (props.definition.round !== round && timerExpired) {
+            setShowTimerExpiredOverlay(true);
+        }
+        setRound(props.definition.round);
+    }, [props.definition.round, round, timerExpired]);
+
+    let timerClass = `fa-fw p-4 text-3xl ${timerIconColor}`;
+
+    if (showTimerExpiredOverlay) {
+        timerClass = "invisible";
+    }
 
     return (
         <div>
             <div className="absolute right-0 top-0 z-5">
                 <FontAwesomeIcon icon="stopwatch" className={timerClass} onClick={configureTimer}/>
             </div>
-            <TimerConfig show={showTimerConfig} engageTimer={engageTimer}/>
-            <TimeoutOverlay show={timerExpired}/>
+            <TimerConfig show={showTimerConfigOverlay} engageTimer={engageTimer}/>
+            <TimeoutOverlay show={showTimerExpiredOverlay}/>
         </div> 
     );
 }
--- models/challenge_specification.ts
+++ models/challenge_specification.ts
@@ -1,4 +1,5 @@
 export default interface ChallengeSpecification {
+    round: number;
     number1: number;
     number2: number;
     result: number;
