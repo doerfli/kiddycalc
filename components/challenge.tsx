dad4ab1c873f3a156eb5168c6d46588958dabe48
Marc Doerflinger <marc@doerf.li>
2022-10-09 10:11:15 +0000

    add subtraction (closes #8)

--- components/challenge.tsx
+++ components/challenge.tsx
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { GameContext } from "../models/game_context";
 import InputBlock from "./input_block";
+import { Operation } from "../models/challenge_specification";
 
 const ResultSelector = dynamic(
     () => import('./result_selector'),
@@ -16,13 +17,29 @@ interface ChallengeProps {
 export default function Challenge(props: ChallengeProps) {
     const { gameState } = useContext(GameContext) as GameContext;
     
+    let operator;
+
+    switch (gameState.challenge.operation) {
+        case Operation.ADDITION:
+            operator = (<FontAwesomeIcon icon="plus" className="icon_operator" />);
+            break;
+
+        case Operation.SUBTRACTION:
+            operator = (<FontAwesomeIcon icon="minus" className="icon_operator" />);
+            break;
+
+        default:
+            operator = (<FontAwesomeIcon icon="plus" className="icon_operator" />);
+            break;
+    }
+
     return (
         <div className="challenge">
             <div className="challenge_element">
                 <InputBlock number={gameState.challenge.number1} icon={gameState.challenge.icon} type={gameState.challenge.inputType1} />
             </div>
             <div className="challenge_element">
-                <FontAwesomeIcon icon="plus" className="icon_operator" />
+                {operator}
             </div>
             <div className="challenge_element">
                 <InputBlock number={gameState.challenge.number2} icon={gameState.challenge.icon} type={gameState.challenge.inputType2} />
--- models/challenge_specification.ts
+++ models/challenge_specification.ts
@@ -1,9 +1,15 @@
 import { NumberElementType } from "../components/number/number_element";
 import { getRandomIcon } from "../utils/icons";
 
+export enum Operation {
+    ADDITION,
+    SUBTRACTION
+}
+
 export default interface ChallengeSpecification {
     number1: number;
     number2: number;
+    operation: Operation;
     result: number;
     icon: string;
     inputType1: NumberElementType;
@@ -27,13 +33,41 @@ export const newChallengeAddition = (max: number, allowZero = false): ChallengeS
     let n2;
 
     do {
-        n2 = Math.floor(Math.random() * (max));
+        n2 = Math.floor(Math.random() * max);
     } while (n1 + n2 > max || (! allowZero && n2 == 0));
 
     return {
         number1: n1,
         number2: n2,
+        operation: Operation.ADDITION,
         result: n1 + n2,
+        icon: getRandomIcon(),
+        inputType1: getRandomNumberElementType(),
+        inputType2: getRandomNumberElementType(),
+        resultType1: getRandomNumberElementType(),
+        resultType2: getRandomNumberElementType(),
+        resultType3: getRandomNumberElementType()
+    };
+}
+
+export const newChallengeSubtraction = (max: number): ChallengeSpecification => {
+    let n1;
+
+    do {
+        n1 = Math.floor(Math.random() * max);
+    } while (n1 <= 2 || n1 > max); // allow 1 < n1 < max
+
+    let n2;
+
+    do {
+        n2 = Math.floor(Math.random() * max);
+    } while (n2 < 1 || n2 >= n1);
+
+    return {
+        number1: n1,
+        number2: n2,
+        operation: Operation.SUBTRACTION,
+        result: n1 - n2,
         icon: getRandomIcon(),
         inputType1: getRandomNumberElementType(),
         inputType2: getRandomNumberElementType(),
@@ -50,12 +84,23 @@ export const newChallengeType1 = (): ChallengeSpecification => {
 
 /* type 2 challenge has a sum of maximum 15 */
 export const newChallengeType2 = (): ChallengeSpecification => {
-    return newChallengeAddition(15);
+    const r = Math.random();
+    if (r < 0.2) {
+        return newChallengeSubtraction(10);
+    } else {
+        return newChallengeAddition(15);
+    }
 }
 
 export const newChallengeType3 = (): ChallengeSpecification => {
-    return newChallengeAddition(20);
+    const r = Math.random();
+    if (r < 0.4) {
+        return newChallengeSubtraction(15);
+    } else {
+        return newChallengeAddition(20, true);
+    }
 }
+
 
 export const newChallenge = (level: number): ChallengeSpecification => {
     switch (level) {
